f3e7707cc9e5634b8e0fb94f67937f7ac489709e
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2022-03-14 16:41:12 +0530

    Added Curve

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,22 @@
 const Create = () => {
     return ( <div className="create">
         <h2>Add a new Blog</h2>
+        <form>
+            <label>Blog Title</label>
+            <input
+            type="text"
+            required
+            />
+            <label>Blog Body</label>
+            <textarea
+            required></textarea>
+            <label>Blog author:</label>
+            <select>
+                <option value="Madara">Madara</option>
+                <option value="Rengoku">Rengoku</option>
+            </select>
+            <button>Add Blog</button>
+        </form>
     </div>);
 }
  
