93ea9cdfef6991343d1ef2d93045cee7803b0fbf
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2022-03-16 21:13:59 +0530

    Creating blogs enabled

--- src/Create.js
+++ src/Create.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( <div className="create">
-        <h2>Add a new Blog</h2>
-        <form>
-            <label>Blog Title</label>
-            <input
-            type="text"
-            required
-            />
-            <label>Blog Body</label>
-            <textarea
-            required></textarea>
-            <label>Blog author:</label>
-            <select>
-                <option value="Madara">Madara</option>
-                <option value="Rengoku">Rengoku</option>
-            </select>
-            <button>Add Blog</button>
-        </form>
-    </div>);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleSubmit =(e) =>{
+      e.preventDefault();
+      const blog ={title,body,author};
+      console.log(blog);
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        
+      </form>
+    </div>
+  );
 }
  
 export default Create;
